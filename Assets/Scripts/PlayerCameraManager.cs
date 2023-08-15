4a54bbf3071cf6df37559a801204db1c4cdbf33b
Ravenovus <voronov213@gmail.com>
2023-08-15 12:54:47 +0300

    Camera System Rework
    The camera has been entirely reworked to use Cinemachine Virtual Camera.
    A camera manager object and script were added.
    
    At the moment of the commit, the project requires a lot of additional setup.
    Personal Recommendation:
    In the main branch, on the owner's PC, rebuild the camera system and then upload the project entirely.

---
+++ Assets/Scripts/PlayerCameraManager.cs
@@ -0,0 +1,22 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraManager : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera playerCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetFollowCamera();
+    }
+
+    private void SetFollowCamera()
+    {
+        playerCamera = GetComponent<CinemachineVirtualCamera>();
+        playerCamera.Follow = GameObject.FindWithTag("Player").transform;
+    }
+}
