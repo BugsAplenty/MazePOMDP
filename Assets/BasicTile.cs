c30df3f9cf3babc0de3b5c9c581fd5028ab7fe14
Ravenovus <voronov213@gmail.com>
2023-08-16 22:31:21 +0300

    Ignore this commit

--- Assets/BasicTile.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-[CreateAssetMenu(fileName = "NewTile", menuName = "Tiles/Basic Tile")]
-public class BasicTile : Tile
-{
-    // You can add additional properties and methods here if needed
-}
